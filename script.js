f6fd3ce3e18d5a74974756b71f2a5946ce750a6b
Pablo Vinícius Sousa Silva <48532322+PabloViniciusSS@users.noreply.github.com>
2020-09-12 19:50:30 -0300

    reorganizar

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let canvas = document.getElementById("snake");
-let Context = canvas.getContext("2d");
+let context = canvas.getContext("2d");
 let box = 32; 
 let snake = [];
 snake[0] = {
@@ -7,21 +7,33 @@ snake[0] = {
     y: 8 * box
 }
 
+let direction ="right";
+
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
 function criarBG(){
     context.fillStyle ="lightgreen";
-    context.fillRect(0,0,16 * box, 16 * box)
+    context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
         context.fillStyle = "green";
-        context.fillRect(snake[i].x, snake[i]. box, box);
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
 
-function update(event){
+function update (event){
     if(event.KeyCode == 37 && direction != "right") direction = "left";
     if(event.KeyCode == 38 && direction != "down") direction = "up";
     if(event.KeyCode == 39 && direction != "left") direction = "right";
@@ -30,29 +42,50 @@ function update(event){
 }
 
 function iniciarJogo(){
+    
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over:');
+        }
+    }
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
 
-    let snakex = snake[0].x;
-    let snakey = snake[0].y;
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
     
-    if(direction == "right") snakex += box;
-    if(direction == "left") snakex -= box;
-    if(direction == "up") snakey -= box;
-    if(direction == "down") snakey += box;
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
 
-    snake.pop();
+
+    if(snakeX != food.x || snakeY != food.y){
+
+         snake.pop();
+
+    } else{
+
+    food.x = Math.floor(Math.random() * 15 + 1) * box,
+     food.y = Math.floor(Math.random() * 15 + 1) * box
+}
+    
+
+
+   
 
     let newHead = {
-        x: snakex,
-        y: snakey
+        x: snakeX,
+        y: snakeY
     }
 
     snake.unshift(newHead)
